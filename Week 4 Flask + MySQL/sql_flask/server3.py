8a63d6c54ae849745a703f2b60ca59632803805a
nnijiati <nadia831@live.com>
2019-01-19 17:04:20 -0500

    got mysql working

--- Week 4 Flask + MySQL/sql_flask/server3.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-from mysqlconnection import connectToMySQL
-
-app = Flask(__name__)
-
-db = connectToMySQL(db="mydb_janurary")
-query = "SELECT * FROM  users;"
-print (db.query_db(query))
